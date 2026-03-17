aaeb31555efd93804544528c6b358c1bd061a78c
Sadik Mohamud <msadiknur@gmail.com>
2026-03-17 04:07:33 +0000

    Register Category and Product in Django admin

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "merchant", "category", "price", "stock", "is_active")
+    list_filter = ("category", "is_active")
+    search_fields = ("name", "merchant__email")
